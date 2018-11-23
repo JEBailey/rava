0917b6001df262214ec2a159d55ab249e302f8be
JE Bailey <jason.bailey@sas.com>
2018-11-23 16:59:49 -0500

    committing initial documentation

---
+++ docs/js/rava.js
@@ -0,0 +1,177 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+/* eslint-env es6, browser */
+(function() {
+    var rava = {};
+    var tagSelectors = {};
+    var elementMap = new WeakMap ();
+
+    if (!Element.prototype.matches) {
+        Element.prototype.matches = Element.prototype.msMatchesSelector;
+    }
+
+    if (typeof NodeList.prototype.forEach !== "function"
+            && typeof Array.prototype.forEach === "function") {
+        NodeList.prototype.forEach = Array.prototype.forEach;
+    }
+
+    rava.decorate = function(selector, config) {
+        tagSelectors[selector] = config;
+        document.querySelectorAll (selector).forEach (function(node) {
+            wrap (node, config);
+        });
+    };
+
+    new MutationObserver (function(mutations) {
+        mutations.forEach (function(mutation) {
+            traverse(mutation.addedNodes,wrap);
+            traverse(mutation.removedNodes,remove);
+        });
+    }).observe (document.body, {
+        attributes : false,
+        childList : true,
+        subtree : true,
+        characterData : false
+    });
+
+    var traverse = function(nodeList, callback){
+        nodeList.forEach (function(node){
+            var checkSet = new Set ();
+            checkSet.add (node);
+            checkSet.forEach (function(foundElement) {
+                if (foundElement.querySelectorAll) {
+                    for ( var selector in tagSelectors) {
+                        if (foundElement.matches (selector)) {
+                            callback (foundElement, tagSelectors[selector]);
+                        }
+                    }
+                }
+                var foundElements = foundElement.children;
+                if (foundElements) {
+                    for (var i = 0; i < foundElements.length; i++) {
+                        checkSet.add (foundElements[i]);
+                    }
+                }
+                checkSet["delete"] (foundElement);
+            });
+        });
+    };
+
+    var wrap = function(node, config) {
+        var configSet = elementMap.get (node);
+        if (!configSet) {
+            configSet = new Set ();
+            elementMap.set (node, configSet);
+        }
+        if (configSet.has (config)) {
+            if (config.callbacks.added) {
+                config.callbacks.added.call (node);
+            }
+            return;
+        }
+        configSet.add (config);
+
+        var names = Object.getOwnPropertyNames (config);
+        var data = config.data;
+        names.forEach (function(name) {
+            if (name === "constructor") {
+                return;
+            }
+            if (name === "events") {
+                registerEventHandlers (node, data, config[name]);
+            }
+            if (name === "methods") {
+                handleMethods (node, config[name]);
+            }
+            if (name === "data") {
+                node.data = config.data;
+            }
+        });
+        if (config.callbacks) {
+            if (config.callbacks.created) {
+                config.callbacks.created.call (node);
+            }
+            if (config.callbacks.added) {
+                config.callbacks.added.call (node);
+            }
+        }
+    };
+
+    var remove = function(node,config){
+        var configSet = elementMap.get (node);
+        if (!configSet) {
+            return;
+        }
+        if (configSet.has (config)) {
+            if (config.callbacks && config.callbacks.removed) {
+                config.callbacks.removed.call (node);
+            }
+            return;
+        }
+    };
+
+    var targetedEventHandler = function(fn, correctTarget, data) {
+        return function(event) {
+            if (!event.target.matches (correctTarget)) {
+                return;
+            }
+            fn.call (this, event, data);
+        };
+    };
+
+    var handleMethods = function(node, funcs) {
+        for ( var funcName in funcs) {
+            node[funcName] = funcs[funcName].bind(node);
+        }
+    };
+
+    var registerEventHandlers = function(node, data, events) {
+        for ( var eventName in events) {
+            var possibleFunc = events[eventName];
+            var targetNode = node;
+            if (typeof possibleFunc !== "object") {
+                targetNode.addEventListener (eventName, function(event) {
+                    possibleFunc.call (node, event, data);
+                });
+            } else {
+                var selector = eventName;
+                for ( var childEventName in possibleFunc) {
+                    var func = targetedEventHandler (
+                            possibleFunc[childEventName], selector, data);
+                    targetNode.addEventListener (childEventName, (function(
+                            func, node, data) {
+                        return function(event) {
+                            func.call (node, event, data);
+                        }
+                    }) (func, node, data));
+                }
+            }
+        }
+    };
+
+    if (typeof define === 'function' && define.amd) {
+        define (rava);
+    } else if (typeof module !== 'undefined' && module.exports) {
+        module.exports = {
+            rava : rava
+        };
+    } else {
+        window.rava = rava;
+    }
+})();
---
+++ docs/js/scripts.js
@@ -0,0 +1,102 @@
+rava.decorate("#nav li",{
+    events:{
+        click: function(event) {
+            this.parentElement.querySelectorAll(":scope li").forEach(element => {
+                element.classList.remove("is-active");
+            });
+            this.classList.add("is-active");
+        }
+    }
+});
+
+rava.decorate("section.hero",{
+    events:{
+        "#nav li *" : {
+            click: function(event) {
+                var targetId = event.target.closest("li").dataset.target;
+                var tabs = this.querySelectorAll(":scope .tab-pane");
+                tabs.forEach(function(tab) {
+                    if (tab.id == targetId) {
+                      tab.style.display = "block";
+                      tab.classList.add("is-active");
+                    } else {
+                      tab.style.display = "none";
+                      tab.classList.remove("is-active");
+                    }
+                });
+            }
+        }
+    }
+});
+
+rava.decorate(".burger", {
+    callbacks: {
+        created : function(){
+            this.burgerTarget = document.querySelector('#'+this.dataset.target);
+        }
+    },
+    events : {
+        click: function(){
+            this.burgerTarget.classList.toggle('is-active');
+            this.classList.toggle('is-active');
+        }
+    }
+});
+
+rava.decorate(".remote-content", {
+    callbacks: {
+        created : function(){
+            var currentElement = this;
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+              if (this.readyState == 4 && this.status == 200) {
+                currentElement.innerHTML = this.responseText;
+              }
+            };
+            xhttp.open("GET", this.dataset.target, true);
+            xhttp.send();
+        }
+    }
+});
+
+//first rava example
+rava.decorate('rava-time',{
+    callbacks:{
+        created: function(){
+            this.start();
+        }
+    },
+    methods : {
+        start : function() {
+            this.tick();
+            this.__timeInterval = setInterval(this.tick,1000);
+        },
+        stop : function() {
+            this.__timeInterval = clearInterval(this.__timeInterval);
+        },
+        tick : function() {
+            this.textContent = new Date().toLocaleTimeString();
+        }
+    },
+    events : {
+        click : function() {
+            if (this.__timeInterval){
+                this.stop();
+            } else {
+                this.start();
+            }
+        }
+    }
+});
+
+//this isn't rava based
+window.onload = function() {
+    Particles.init({
+      selector: '.background',
+      maxParticles: 40,
+      sizeVariations : 6,
+      minDistance: 60,
+      connectParticles : true
+    });
+  };
+
