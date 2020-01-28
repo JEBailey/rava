5106d20878ef38d170072c3e4ad34ea9042b7ab4
JE Bailey <jason.bailey@sas.com>
2020-01-28 16:49:09 -0500

    Update to handle scoped and target configurations

--- docs/js/rava.js
+++ docs/js/rava.js
@@ -37,17 +37,16 @@
         return this.substr(position, searchString.length) === searchString;
     };
     //polyfilling for template literals
-    var format = function( node, string){
+    var format = function( el, string){
         var regex = /{(\w+)}/g;
         var found = string.match(regex);
         if (found){
             found.forEach(
                 function(found){
                     var oldValue = found.slice(1,found.length-1);
-                    if(oldValue in node.dataset){
-                        string = string.replace(found, node.dataset[oldValue]);
-                    } else {
-                        console.log("unable to find data-" + oldValue + "in element " + node);
+                    var newValue = el.dataset[oldValue];
+                    if(newValue){
+                        string = string.replace(found,newValue);
                     }
                 }
             )
